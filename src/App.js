950db604beb1857063f22a7dcb450435154a0aba
Efoneli <abbaefoneli@gmail.com>
2023-01-20 08:00:55 +1030

    landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo1 from './assets/logo-1.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="h-screen px-7 bg-[url(./assets/house.png)]">
+      <nav className='flex p-4'>
+      <img src={ logo1 } alt="Logo" className='m-4'/>;
+        <div className='flex justify-between itemscenter m-4 ml-5 text-white '>
+          <li className='m-5'>Home</li>
+          <li className='m-5'>Landlord</li>
+          <li className='m-5'>Tenants</li>
+          <li className='m-5'>Contact Us</li>
+        </div>
+      </nav>
+      <hr />
+      <div>
+      <div className='text-white font-bold text-3xl my-5'>
+        <h1>The Most Affordable<br/>Place To Stay In The <br/>San Francisco Bay Area</h1>
+      </div>
+      <div>
+
+      </div>
+      </div>
+      </div>
+      <div className=''>
+        <h2 className='text-2xl font-bold'>Minimum Living Cost Takes Care Of Everthing</h2>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 }
