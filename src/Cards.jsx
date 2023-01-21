2a6fc52e5699e9864271d6877caf84ed01c7d9da
Efoneli <abbaefoneli@gmail.com>
2023-01-22 02:46:47 +1030

    cards

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -8,43 +8,150 @@ import Bed from './assets/Bed.svg';
 function Cards(props) { 
   return (
     <div className='mx-11'>
-        <div className='flex justify-between  my-6 bg-red-500'>
+        <div className='flex justify-between  my-6 '>
           <h1 className='text-2xl font-bold'>List Of Properties</h1>
-          <button className='bg-orange-700 text-white text-sm rounded-sm px-3 py-2'>
+          <button className='bg-orange-700 text-white text-sm rounded-lg px-3 py-2'>
             View All Property
           </button>
         </div>
-        <div className=''>
-            <div className='bg-white h-[1/3] w-[250px] rounded-lg my-5 shadow-md shadow-black'>
+        <div className='container grid gap-2 place-items-center lg:grid-cols-3  md:grid-cols-3 sm:grid-cols-1'>
+            <div className='bg-white h-[1/3] w-[54vh] rounded-lg my-5 shadow-lg shadow-black'>
                 <img src={bedroom} alt='bedroom' className='object-cover'/>
                 <h2 className='font-bold text-xl py-2 px-3'>
-                2578 Folsom Street, San Francisco, CA, 94110'
+                2578 Folsom Street, San Francisco, CA, 94110
                 </h2>
                 <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
-                <p className='text-orange-500 font-bold pb-4 px-3'>$1200/Month</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
                 <div className='flex justify-around items-center border-t-2'>
                   <div className='flex justify-around items-center border-r-2 px-4 py-5'>
                   <img src={Bed} alt='icon' />
-                    <p className='p-2'>4</p>
+                    <p className='p-2 font-bold text-xl'>4</p>
                   </div>
                   <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
                   <img src={Shower} alt='icon' />
-                    <p className='p-2'>2</p>
+                    <p className='p-2 font-bold text-xl'>2</p>
                   </div>
                   <div className='flex justify-around items-center p-4'>
                   <img src={Size} alt='icon' />
-                    <p className='p-2'>2</p>
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                </div>
+            </div>
+            
+            <div className='bg-white h-[1/3] w-[350px] rounded-lg my-5 shadow-md shadow-black'>
+                <img src={bedroom} alt='bedroom' className='object-cover'/>
+                <h2 className='font-bold text-xl py-2 px-3'>
+                2578 Folsom Street, San Francisco, CA, 94110
+                </h2>
+                <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
+                <div className='flex justify-around items-center border-t-2'>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5'>
+                  <img src={Bed} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>4</p>
+                  </div>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
+                  <img src={Shower} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                  <div className='flex justify-around items-center p-4'>
+                  <img src={Size} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
                   </div>
                 </div>
             </div>
 
-        {/* <ul>
-      {props.data.map((data) => (
-        <li key={data.id}>{data.title}</li>
-      ))}
-    </ul> */}
-            {/* <img src={props.image} alt='image'/>
-            <h1>{props.title}</h1> */}
+            <div className='bg-white h-[1/3] w-[350px] rounded-lg my-5 shadow-md shadow-black'>
+                <img src={bedroom} alt='bedroom' className='object-cover'/>
+                <h2 className='font-bold text-xl py-2 px-3'>
+                2578 Folsom Street, San Francisco, CA, 94110
+                </h2>
+                <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
+                <div className='flex justify-around items-center border-t-2'>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5'>
+                  <img src={Bed} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>4</p>
+                  </div>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
+                  <img src={Shower} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                  <div className='flex justify-around items-center p-4'>
+                  <img src={Size} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                </div>
+            </div>
+
+            <div className='bg-white h-[1/3] w-[350px] rounded-lg my-5 shadow-md shadow-black'>
+                <img src={bedroom} alt='bedroom' className='object-cover'/>
+                <h2 className='font-bold text-xl py-2 px-3'>
+                2578 Folsom Street, San Francisco, CA, 94110
+                </h2>
+                <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
+                <div className='flex justify-around items-center border-t-2'>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5'>
+                  <img src={Bed} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>4</p>
+                  </div>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
+                  <img src={Shower} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                  <div className='flex justify-around items-center p-4'>
+                  <img src={Size} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                </div>
+            </div>
+
+            <div className='bg-white h-[1/3] w-[350px] rounded-lg my-5 shadow-md shadow-black'>
+                <img src={bedroom} alt='bedroom' className='object-cover'/>
+                <h2 className='font-bold text-xl py-2 px-3'>
+                2578 Folsom Street, San Francisco, CA, 94110
+                </h2>
+                <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
+                <div className='flex justify-around items-center border-t-2'>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5'>
+                  <img src={Bed} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>4</p>
+                  </div>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
+                  <img src={Shower} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                  <div className='flex justify-around items-center p-4'>
+                  <img src={Size} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                </div>
+            </div>
+
+            <div className='bg-white h-[1/3] w-[350px] rounded-lg my-5 shadow-md shadow-black'>
+                <img src={bedroom} alt='bedroom' className='object-cover'/>
+                <h2 className='font-bold text-xl py-2 px-3'>
+                2578 Folsom Street, San Francisco, CA, 94110
+                </h2>
+                <p className='text-sm text-gray-300  pt-2 px-3'>Private Room</p>
+                <p className='text-orange-500 font-bold text-xl pb-4 px-3'>$1200/Month</p>
+                <div className='flex justify-around items-center border-t-2'>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5'>
+                  <img src={Bed} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>4</p>
+                  </div>
+                  <div className='flex justify-around items-center border-r-2 px-4 py-5 '>
+                  <img src={Shower} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                  <div className='flex justify-around items-center p-4'>
+                  <img src={Size} alt='icon' />
+                    <p className='p-2 font-bold text-xl'>2</p>
+                  </div>
+                </div>
+            </div>
         </div>
     </div>
   )
