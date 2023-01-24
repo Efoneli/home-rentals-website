2d1964b050dd130b30b674e129ce5da50a1374dc
Efoneli <abbaefoneli@gmail.com>
2023-01-25 07:17:56 +1030

    formik

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,18 +1,80 @@
-import React from 'react'
+import React from 'react';
+import { Formik } from 'formik';
 
-function Form() {
-  return (
-    <div>
-        <div>
-            <h2>Add A New Property</h2>
-            <form>
-                <label>Name
-                    <input placeholder='Enter Name' value='text'/>
-                </label>
-            </form>
-        </div>
-    </div>
-  )
-}
+const Form = () => (
+  <div>
+    <h1>Add A New Property</h1>
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+            <input
+            type="text"
+            name="name"
+            placeholder='Enter Name'
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.name && touched.name && errors.name}
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
 
-export default Form
+export default Form;
