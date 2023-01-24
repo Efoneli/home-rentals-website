36eb9d18c3e3abedea2e8d0d61832531919b55a9
Efoneli <abbaefoneli@gmail.com>
2023-01-25 06:45:30 +1030

    form created

---
+++ src/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+        <div>
+            <h2>Add A New Property</h2>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
