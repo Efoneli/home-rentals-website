c05aae19bfb3d380d865d7d8103ac30ab52fd755
Efoneli <abbaefoneli@gmail.com>
2023-02-10 01:25:16 +0100

    aadded another section pink

--- src/Pink.jsx
+++ src/Pink.jsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import chairs from './assets/chairs.jpg'
-
+import React from "react";
+import chairs from "./assets/chairs.jpg";
 
 function Pink() {
   return (
-    <div className=' flex bg-green-500 h-screen'>
+    <div className=" flex bg-green-500 h-screen">
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+          interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel
+          pellentesque est. Curabitur at odio sit amet libero vulputate
+          efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa,
+          faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus
+          fringilla finibus.
+        </p>
         <div>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam interdum nisl et nunc facilisis, a commodo eros mollis. Nunc vel pellentesque est. Curabitur at odio sit amet libero vulputate efficitur ac nec justo. Nulla vitae mauris quam. Nulla quam massa, faucibus id pretium ac, mattis eu velit. Donec sed risus a lacus fringilla finibus.
-            </p>
-            <div>
-                <img src='Elipse' alt='elipse' />
-                <div className=''>
-                    <h1>Harry Wilson</h1>
-                    <p>Property Owner</p>
-                </div>
-            </div>
-            <img src={chairs} alt='chair' />
+          <img src="Elipse" alt="elipse" />
+          <div className="">
+            <h1>Harry Wilson</h1>
+            <p>Property Owner</p>
+          </div>
         </div>
+      </div>
+      <img src={chairs} alt="chair" />
     </div>
-  )
+  );
 }
 
-export default Pink
+export default Pink;
